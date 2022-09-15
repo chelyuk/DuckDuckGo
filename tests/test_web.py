00867fe759b1a85c60e5cd5695790461e22dfee9
Sergii_Cheliuk <bormot87@gmail.com>
2022-09-15 18:44:02 +0300

    Remove old files and refactor test

--- tests/test_web.py
+++ tests/test_web.py
@@ -7,8 +7,9 @@ import pytest
 
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
+
+from pages.search import DuckDuckGoSearchPage
+from pages.result import DuckDuckGoResultPage
 
 @pytest.fixture
 def browser():
@@ -27,29 +28,15 @@ def browser():
     driver.quit()
 
 def test_basic_duckduckgo_search(browser):
-    # Set up some test case data
-    URL = 'https://www.duckduckgo.com'
+    
     PHRASE = 'panda'
 
-    # Navigate to the DuckDuckGo home page
-    browser.get(URL)
-
-    # Find the search input element
-    # In the DOM, it has an 'id' attribute of 'search_form_input_homepage'
-    search_input = browser.find_element(By.ID,'search_form_input_homepage')
-
-    # Send a search phrase to the input and hit the RETURN key
-    search_input.send_keys(PHRASE + Keys.RETURN)
-
-    # Verify that results appear on the results page
-    link_divs = browser.find_elements(By.CSS_SELECTOR,'#links > div')
-    assert len(link_divs) > 0
-
-    # Verify that at least one search results contains the search phrase
-    xpath = f"//div[@id='links']//*[contains(text(). '{PHRASE}')]"
-    phrase_results = browser.find_elements(By.XPATH,xpath)
-    assert len(phrase_results) > 0
-
-    # Verify that the search phrase is the same
-    search_input = browser.find_elements(By.ID,'search_form_input')
-    assert search_input.get_attribute('value') == PHRASE
+    search_page = DuckDuckGoSearchPage(browser)
+    search_page.load()
+    search_page.search(PHRASE)
+    
+    result_page = DuckDuckGoResultPage(browser)
+    assert result_page.link_div_count() > 0
+    assert result_page.phrase_result_count(PHRASE) > 0
+    assert result_page.search_input_value() == PHRASE
+    
